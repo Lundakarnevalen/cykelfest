4fc27478e1488b96ebfbdf9aa427bf16406365e7
martinjohansson93 <dat13mjo@student.lu.se>
2018-04-24 19:09:34 +0200

    need to swap la computer

--- index.js
+++ index.js
@@ -7,29 +7,32 @@ const fs = require('fs')
 
 
 // 1. Load all input data
-let input = fs.readFileSync(__dirname+'/input.csv', 'utf-8');
-let data = parse(input, {columns: true});
+let input = fs.readFileSync(__dirname + '/input.csv', 'utf-8');
+let data = parse(input, { columns: true });
 let allEntries = data.map(p => {
   return {
     'time': p['Tidstämpel'],
-    'name': p['Namn'],
-    'attend': p['Jag vill anmäla mig'],
-    'name2': p['(Om anmälan sker i par) - Jag vill anmäla mig tillsammans med'],
-    'address': p['Adress där måltiden ska förtäras'],
-    'phone': p['Telefonnummer till båda i paret (tydligt vems som är vems)'],
-    'afterparty': p['Jag/Vi vill ha förköp till Malmös?'].startsWith('Ja'),
-    'iam': p['Jag är'],
-    'foodpref': p['Matpreferenser på dig/på någon i paret?'],
-    'area': [p['Jag bor (lättare för oss då vi ska försöka minimera cykelsträckan)']],
-    'pepp': p['ÄR DU TAGGAD?!??!']
+    'name': p['Namn 1'],
+    'email1': p['Email 1'],
+    'name2': p['Namn 2'],
+    'email2': p['Email 2'],
+    'name3': p['Namn 3'],
+    'email3': p['Email 3'],
+    'peoples': p['Vi är så här många i vårt lilla gäng!'],
+    'groups': p['Vilken nollningsgrupp'],
+    'area': [p['Adress där maten ska förtäras / Address where the food will be served ']],
+    'phone': p['Telefonnummer till deltagande / Phone number to all attending '],
+    'foodpref': p['Matpreferenser? (specificera för vem, men ändå inte för vem se beskrivning) / Food preferences? (specify for who attending) '],
+    'restrictions': p['Eventuella restriktioner:Ex) Jag har hund, kan endast vara hos mig efter 20, pälsallergiker'],
+    'district': p['Stadsdel där maten ska förtäras / District where the food will be served ']
   }
 })
 
 // 2. Match all singles to pairs. 
 let [pairs, singles] = _.partition(allEntries, p => !!p.name2)
-for(let i = 0; i < singles.length; i+=2){
+for (let i = 0; i < singles.length; i += 2) {
   let a = singles[i]
-  let b = singles[i+1] || {}
+  let b = singles[i + 1] || {}
   let joined = {
     'time': a.time,
     'name': a.name,
@@ -47,7 +50,7 @@ for(let i = 0; i < singles.length; i+=2){
 }
 
 
-function createDistributionList(){
+function createDistributionList() {
   // 3. Shuffle list of pairs
   pairs = _.shuffle(pairs)
 
@@ -55,117 +58,135 @@ function createDistributionList(){
   //    and one for the desert.
   //    A meal will contain a list of pairs. Where the first one is the host,
   //    and the others are the guests of the meal.
-  let nbrOfDates  = parseInt(pairs.length / 3)
+  let nbrOfDates = parseInt(pairs.length / 3)
   let meals = [
     [], // Aptizers
     [], // Maincourse
     [], // Desert
   ]
   // Set a pair (of attendees) as hosts for each meal
-  for(let i = 0; i < pairs.length; i++){
-    if(meals[i%3].length < nbrOfDates){
-      meals[i%3].push([pairs[i]])
+  for (let i = 0; i < pairs.length; i++) {
+    if (meals[i % 3].length < nbrOfDates) {
+      meals[i % 3].push([pairs[i]])
     } else {
-      meals[i%3][0].push(pairs[i])
+      meals[i % 3][0].push(pairs[i])
     }
-  } 
+  }
 
   // 5. Fill all guests in all meal-list. One pair can only be en one meal-list 
   //    once. 
-  meals.forEach( (meal, i) => {
+  meals.forEach((meal, i) => {
     let diff = _.difference(pairs, _.flatten(meal))
     diff = _.shuffle(diff)
-    diff.forEach( (d, idx) => {
-      meals[i][idx%nbrOfDates].push(d)
+    diff.forEach((d, idx) => {
+      meals[i][idx % nbrOfDates].push(d)
     })
-  })  
+  })
   return meals
 }
 // 7. Evaluate function.
 //    Cuz we randomize so we have to evaluate how good a distribution
 //    is. 
 
-function evaluate(mealList){
+function evaluate(mealList) {
   let points = 10;
-  
+
   // Penalty if two pair eat together twice.
   pairs.forEach(pair => {
-    let attendees = mealList.reduce( (acc, curr) => {
-      let meal = curr.filter( c => _.indexOf(c, pair) >= 0 )
+    let attendees = mealList.reduce((acc, curr) => {
+      let meal = curr.filter(c => _.indexOf(c, pair) >= 0)
       return acc.concat(meal)
     }, [])
-    const guestCount = attendees.reduce( (acc, curr) => acc + curr.length, 0)
+    const guestCount = attendees.reduce((acc, curr) => acc + curr.length, 0)
     const set = new Set(_.flatten(attendees))
 
     // If size is less than 7, then two pair have eaten together twice
     // Remove 2 because of the duplicate of the pair in mealLists
     const limit = guestCount - 2
-    if(set.size < limit){
+    if (set.size < limit) {
       points = -1
     }
   })
-  if(points < 0){ // Dont allow this rule to be broken
+  if (points < 0) { // Dont allow this rule to be broken
     return points
   }
 
-  // Add points if all who eat desert together are grouped by afterparty.
-  let dessertSchedule = mealList[2]
-  dessertSchedule.forEach(ds => {
-    let [after, noafter] = _.partition(ds, x => x.afterparty)
-    if(after.length === 0 || noafter.length === 0){
-      points += 1
-    }
-  })
+  // Check if the same group meet eachother sometime, in that case return -1
+  try {
+    mealList.forEach(course => {
+      course.forEach(location => {
+        location.forEach(pair => {
+          let meetings = pair.filter(p => _.indexOf(p, p.groups) >= 0)
+        })
+      })
+    })
+  } catch (e) {
+    return -1
+  }
+
+  // // Add points if all who eat desert together are grouped by afterparty.
+  // let dessertSchedule = mealList[2]
+  // dessertSchedule.forEach(ds => {
+  //   let [after, noafter] = _.partition(ds, x => x.afterparty)
+  //   if(after.length === 0 || noafter.length === 0){
+  //     points += 1
+  //   }
+  // })
 
   // Add points if people stay in same zone during all meals.
   pairs.forEach(pair => {
-    let attendees = mealList.reduce( (acc, curr) => {
-      let meal = curr.filter( c => _.indexOf(c, pair) >= 0 )
+    let attendees = mealList.reduce((acc, curr) => {
+      let meal = curr.filter(c => _.indexOf(c, pair) >= 0)
       return acc.concat(meal)
     }, [])
     let hosts = attendees.map(x => x[0])
 
     // Select the first area of the host as their selected area
     let areas = _.flatten(hosts.map(x => x.area[0]))
-    
+
     let areaChange = 0
-    for(let i = 0; i < areas.length - 2; i++) {
-      if(areas[i] !== areas[i+1]){
+    for (let i = 0; i < areas.length - 2; i++) {
+      if (areas[i] !== areas[i + 1]) {
         areaChange++
       }
     }
+    // Add extra points if one swap zone ones.
+    if (areaChange === 2) {
+      points += 5
+    }
+
     // Add point the fewer times a pair stays in same area
     points += areas.length - 1 - areaChange
   })
-  if(points < 0){
+  if (points < 0) {
     return points
   }
 
-  // Add points if afterparty-people at dessert are in center-zone.
-  mealList.forEach(dl => {
-    points += dl.reduce( (acc, curr) => {
-      if(curr[0].afterparty && _.indexOf(curr[0].area, 'Centralt') >= 0){
-        acc++
-      }
-      return acc
-    }, 0)
-  })
-  return  points;
+  // // Add points if afterparty-people at dessert are in center-zone.
+  // mealList.forEach(dl => {
+  //   points += dl.reduce( (acc, curr) => {
+  //     if(curr[0].afterparty && _.indexOf(curr[0].area, 'Centralt') >= 0){
+  //       acc++
+  //     }
+  //     return acc
+  //   }, 0)
+  // })
+  return points;
 }
 
 // 8. Iterate until the best distribution is found
 let nbrSuccess = 0
 let success = []
-let successLimit = 10 
+let successLimit = 10
 let iteration = 0
 let points = 0
 let topPoint = 0
 let distribution = []
-while(nbrSuccess < successLimit){
-  distribution = createDistributionList() 
+while (nbrSuccess < successLimit) {
+  distribution = createDistributionList()
   points = evaluate(distribution)
 
-  if(points > 0){
+  if (points > 0) {
     nbrSuccess += 1
     success.push([points, distribution])
     topPoint = Math.max(points, topPoint)
@@ -173,7 +194,7 @@ while(nbrSuccess < successLimit){
   }
 
   iteration++
-  if(iteration % 10000 === 0){
+  if (iteration % 10000 === 0) {
     console.log(`Iteration ${iteration}`)
   }
 }
@@ -181,7 +202,7 @@ while(nbrSuccess < successLimit){
 // Sort list by points DESC
 highscore = _.reverse(_.sortBy(success, x => x[0]))
 console.log()
-highscore.forEach( (x, idx) => console.log(`${idx+1}. ${x[0]} reached`))
+highscore.forEach((x, idx) => console.log(`${idx + 1}. ${x[0]} reached`))
 
 console.log()
 console.log()
@@ -195,9 +216,9 @@ let main = topdistribution[1]
 let des = topdistribution[2]
 
 const fields = [
-  'host', 
-  'address', 
-  'area', 
+  'host',
+  'address',
+  'area',
   'GuestPair1',
   'GuestPair2',
   'GuestPair3',
@@ -213,19 +234,19 @@ const fields = [
 ];
 const opts = { fields }
 
-function getCsv(list){
+function getCsv(list) {
   let data = list.map(a => {
     host = a[0]
     g1 = a[1]
     g2 = a[2]
     g3 = a[3]
-    foodpref = a.filter(x => x.foodpref).map( x => `${x.foodpref}`).join(' och ')
+    foodpref = a.filter(x => x.foodpref).map(x => `${x.foodpref}`).join('\n')
     let obj = {
-      host: host.name + ' och ' + host.name2,
+      host: host.name + ', ' + host.name2,
       address: host.address,
       area: host.area.join(', '),
-      GuestPair1: g1.name + ' och ' + g1.name2,
-      GuestPair2: g2.name + ' och ' + g2.name2,
+      GuestPair1: g1.name + ', ' + g1.name2 + ', ' + g1.name3,
+      GuestPair2: g2.name + ', ' + g2.name2 + ', ' + g2.name3,
       GuestPair3: '-',
       'Host phone': host.phone,
       'Guest1 phone': g1.phone,
@@ -234,20 +255,22 @@ function getCsv(list){
       'Afterparty Guest1': g1.afterparty,
       'Afterparty Guest2': g2.afterparty,
       'Foodpref': foodpref,
+      'Emails': host.name1 + ', ' + host.email2 + ', ' + host.email3,
+      'Email text': host.prefs
     }
-    if(g3){
-      obj.GuestPair3 = g3.name + ' och ' + g3.name2
+    if (g3) {
+      obj.GuestPair3 = g3.name + ', ' + g3.name2
       obj['Afterparty Guest3'] = g3.afterparty
       obj['Guest3 phone'] = g3.phone
     }
     return obj
   })
 
-  try{
+  try {
     const parser = new json2csv(opts);
     const csv = parser.parse(data);
     return csv
-  } catch (err ){
+  } catch (err) {
     console.error(err);
     return err
   }
@@ -260,9 +283,9 @@ des_csv = getCsv(des)
 writeCsv(apt_csv, 'apt')
 writeCsv(main_csv, 'main')
 writeCsv(des_csv, 'dessert')
-function writeCsv(csv, name){
-  var stream = fs.createWriteStream("scores/" + highscore[0][0]  + "_" + name + ".csv");
-  stream.once('open', function(fd) {
+function writeCsv(csv, name) {
+  var stream = fs.createWriteStream("scores/" + highscore[0][0] + "_" + name + ".csv");
+  stream.once('open', function (fd) {
     stream.write(csv);
     stream.end();
   });
